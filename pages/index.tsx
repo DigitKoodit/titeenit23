4b41dfb56bc0fb1d19e4cf850843462575b8beaf
Petro Silenius <petro.silenius@gmail.com>
2022-07-26 18:28:31 +0300

    Add Countdown timer to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
+import Countdown from 'components/Countdown';
 
 const Home: NextPage = () => {
+  const titeenitStartDate = new Date('2023-03-17T18:00:00');
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <Head>
@@ -10,14 +12,13 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center -z-20 bg-black">
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center -z-20 bg-black space-y-8">
         <div className="bg-landing bg-cover bg-center blur-sm absolute w-full h-full -z-10" />
         <h1 className="text-6xl font-bold text-white">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+          Titeenit <span className="text-blue-600">Turussa</span>
         </h1>
+
+        <Countdown date={titeenitStartDate} />
       </main>
     </div>
   );
