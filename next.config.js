331488e7d97490e82440fc255584515d9eb229b0
Petro Silenius <petro.silenius@gmail.com>
2022-07-26 19:00:47 +0300

    Enable next/future/image update image domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['digit.fi', 'datateknologerna.org'],
+  },
+  experimental: { images: { allowFutureImage: true } },
+};
