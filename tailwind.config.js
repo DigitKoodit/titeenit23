fe96f8a51bdc4f638bfde228c581b40c20445c35
Petro Silenius <petro.silenius@gmail.com>
2022-07-26 18:01:39 +0300

    Add custom landing page image to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        landing:
+          'url(https://digit.kuvat.fi/kuvat/2016/TiTeenit%202016%20%40Turku/antinkuvat/DSC_0698.JPG?img=img2048)',
+      },
+    },
   },
   plugins: [],
-}
+};
