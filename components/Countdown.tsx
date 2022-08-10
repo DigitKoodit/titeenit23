d545e8eac8e75a28d722d81b3c0f21706634728a
Petro Silenius <petro.silenius@gmail.com>
2022-08-10 21:33:54 +0300

    Switch hard coded text to language files, fetch language files on build

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -1,10 +1,14 @@
+import { useTranslation } from 'next-i18next';
+
 const Countdown = ({ date }: { date: Date }) => {
+  const { t } = useTranslation();
+
   const diffInMS = date.getTime() - Date.now();
   const diffInDays = Math.ceil(diffInMS / (1000 * 60 * 60 * 24));
 
   return (
     <div className="flex flex-col items-center justify-center text-3xl font-bold">
-      {diffInDays} päivän päästä
+      {t('x-days-from-now', { days: diffInDays })}
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,38 @@
+import type { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Countdown from 'components/Countdown';
 import { useState } from 'react';
 import BackgroundImage from 'components/BackgroundImage';
 import Footer from 'components/Footer';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useTranslation } from 'next-i18next';
 
 const Home = () => {
+  const { t } = useTranslation('common');
+
   const [buttonText, setButtonText] = useState('');
 
   const titeenitStartDate = new Date('March 17 2023 18:00:00 GMT+0200');
 
   const checkCity = async () => {
-    const fromTurkuCheckResponse = await fetch('/api/requestFromTurku');
-    const fromTurkuCheck = await fromTurkuCheckResponse.json();
+    const cityCheckresponse = await fetch('/api/requestFromTurku');
+    const cityCheck = await cityCheckresponse.json();
 
-    setButtonText(fromTurkuCheck.message);
+    const newButtonText = cityCheck.fromTurku ? t('yes-you-are') : t('not-yet');
+    setButtonText(newButtonText);
   };
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <Head>
-        <title>Turun Titeenit 2023</title>
+        <title>{t('title')}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main
         className="flex flex-1 flex-col items-center justify-center px-20 text-center space-y-8 text-teal-600"
         style={{ textShadow: '2px 2px 1px #1e293b' }}>
         <div>
-          <h1 className="text-6xl font-bold ">Titeenit Turussa</h1>
+          <h1 className="text-6xl font-bold ">{t('heading')}</h1>
           <p className="text-xl font-bold">17.-19.3.2023</p>
         </div>
 
@@ -36,7 +42,7 @@ const Home = () => {
           <button
             className="bg-slate-200 hover:bg-slate-300 transition-colors  font-bold py-2 px-4 my-4 rounded"
             onClick={checkCity}>
-            Olenko Turussa?
+            {t('in-turku-button')}
           </button>
           <p className="text-2xl font-bold">{buttonText}</p>
         </div>
@@ -45,6 +51,14 @@ const Home = () => {
       <BackgroundImage />
     </div>
   );
+};
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale as string, ['common'])),
+    },
+  };
 };
 
 export default Home;
