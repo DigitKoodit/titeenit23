e25a2b20d895b28c198395545e532b62243d6170
Matias Lappalainen <matias.lappalainen@mehilainen.fi>
2022-08-14 20:56:43 +0300

    Fix blur making white drop shadow in corners

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -1,6 +1,7 @@
 const BackgroundImage = () => {
   return (
     <>
+      <div className="bg-black absolute w-full h-full -z-20" />
       <div className="bg-blue-900 opacity-50 absolute w-full h-full z-[-9]" />
       <div className="bg-landing bg-cover bg-center blur-sm absolute w-full h-full -z-10" />
     </>
