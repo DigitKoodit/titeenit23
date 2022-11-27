6826c72ca6220687156a9477ce9246935a397b71
Petro Silenius <petro.silenius@gmail.com>
2022-11-27 23:27:23 +0200

    Fix prettier linting error

--- components/CheckCityButton.tsx
+++ components/CheckCityButton.tsx
@@ -24,7 +24,8 @@ const CheckCityButton = () => {
     <div>
       <button
         className="bg-neutral-100 text-orange-700 transition-colors font-bold py-2 px-4 my-4 rounded hover:bg-orange-700 hover:text-neutral-100 duration-150 ease-in"
-        onClick={checkCity}>
+        onClick={checkCity}
+      >
         {t('in-turku-button')}
       </button>
       <p className="text-orange-500 text-2xl font-bold">{buttonText}</p>
